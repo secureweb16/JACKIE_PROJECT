18f7f8fb16b6d2daedb6ca37af32771b03d531d5
secureweb16 <secureweb16@gmail.com>
2025-02-03 18:38:30 +0530

    fixed authentication issue

--- app/components/AuthCheck.tsx
+++ app/components/AuthCheck.tsx
@@ -4,21 +4,37 @@ import { useRouter } from "next/navigation";
 
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-
+  const getCookie = (name: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+    return null;
+  };
   useEffect(() => {
     const checkAuth = async () => {
-      const token = document.cookie.includes("authToken"); 
 
-      if (!token) {
+       const token = getCookie("authToken");
+      const isAuthPage = window.location.pathname === "/login" || window.location.pathname === "/register";
+
+      if (!token && !isAuthPage) {
         router.push("/login");
+      } 
+      if(!token && isAuthPage) {
+        return;
       }
+      if(token && isAuthPage) {
+        router.push("/dashboard");
+      }
+  
     };
 
-    checkAuth(); 
-    const interval = setInterval(checkAuth, 60000); 
+   
+   checkAuth();
+   const interval = setInterval(checkAuth, 60000); 
 
-    return () => clearInterval(interval); 
-  }, [router]);
+   return () => clearInterval(interval); 
+
+  }, [router]); // Run this effect when the router object is ready
 
   return <>{children}</>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+
+"use client";
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  
+  const token = localStorage.getItem('authToken');
+
+  if (token) {
+    redirect('/dashboard');
+  }
   redirect('/login');
 }
