40e5ab00ee01a223daed65e340f128e225a3b811
secureweb16 <secureweb16@gmail.com>
2025-02-06 12:23:25 +0530

    comment fetchfiledetails api

--- app/report/[id]/page.tsx
+++ app/report/[id]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import React, { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
@@ -59,7 +60,7 @@ const Page = () => {
 
   useEffect(() => {
     if (id) {
-      fetchFileDetails(); 
+      // fetchFileDetails(); 
     }
   }, [id]); 
 
